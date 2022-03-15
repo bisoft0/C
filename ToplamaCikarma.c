ed75ddd700713410e08d173d6c83c9e2dbfcf25c
BilalGYNK <69201444+BilalGYNK@users.noreply.github.com>
2022-03-15 18:59:47 +0300

    Proje dosyası ekle.

---
+++ ToplamaCikarma.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main()
+{
+    int toplam,carpim;
+
+    toplam=8+4;
+    carpim=8*4;
+
+    printf("Toplama=%d Carpma=%d",toplam,carpim);
+
+}
---
+++ ortalama.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main()
+{
+    int a,b;
+    float ortalama;
+
+    printf("Lütfen notarlınızı giriniz===>");
+    scanf("%d %d",a,b);
+
+    ortalama=(a+b)/2;
+
+    printf("Ortalamanız===> %f",ortalama);
+}
